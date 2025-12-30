cccc3c649b688944e6a2b288444901eaeda0fefe
Nabeh Kallas <nabehkallas777@gmail.com>
2025-12-30 06:04:59 +0300

    edits

--- src/components/pages/ResumePage.jsx
+++ src/components/pages/ResumePage.jsx
@@ -4,7 +4,7 @@ function Resume() {
   return (
       <div style={{ display: 'flex', justifyContent: 'center', padding: '20px' }}>
         <img 
-          src="/Resume_CV_png.png" 
+          src="/NabehKallasResume.png" 
           alt="Resume"
           style={{ maxWidth: '100%', height: 'auto' }}
         />
